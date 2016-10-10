06e59652d379322921ba4fee394f3cc516718dcc
Chris <orthagonal1@gmail.com>
2016-10-10 11:18:54 -0500

    switch to use wreck

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const defaults = require('lodash.defaults');
-const request = require('request');
+const wreck = require('wreck');
+
 const defaultOptions = {
   verbose: false,
-  // todo: should these be env variables?
-  host: 'localhost', // the micro-mail host
-  port: 8080, // the micro-mail port
+  host: 'http://thing1:8080' // the micro-mail host
 };
 
 exports.register = function(server, options, next) {
   options = defaults(options, defaultOptions);
-  const hostAddress = `http://${options.host}:${options.port}/send`;
+  const hostAddress = `${options.host}/send`;
   server.decorate('server', 'sendEmail', (data, done) => {
     if (options.verbose) {
       server.log(['debug', 'hapi-micro-mail'], data);
       server.log(['debug', 'hapi-micro-mail'], `sending request to ${hostAddress}`);
     }
-    request({ method: 'POST', url: hostAddress, form: data }, (err, response, body) => {
+    wreck.post(hostAddress, { payload: data }, (err, response, payload) => {
       if (err) {
         server.log(['error', 'hapi-micro-mail'], err);
       }
       if (options.verbose) {
         server.log(['debug', 'hapi-micro-mail'], response);
       }
-      done(JSON.parse(body));
+      done(JSON.parse(payload.toString()));
     });
   });
   next();
