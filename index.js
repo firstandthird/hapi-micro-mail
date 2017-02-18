6135750a2dc5cfd707b1b77315a75c509d24395f
Chris <orthagonal1@gmail.com>
2017-02-18 10:07:02 -0600

    handle micro-mail error responses better

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+'use strict';
 const wreck = require('wreck');
+const Boom = require('boom');
 const defaultOptions = {
   verbose: false,
   host: '' // the micro-mail host
@@ -16,21 +18,45 @@ exports.register = function(server, options, next) {
     }
     wreck.post(hostAddress, { payload: data }, (err, response, payload) => {
       if (err) {
-        return server.log(['error', 'hapi-micro-mail'], err);
+        server.log(['error', 'hapi-micro-mail'], err);
+        return done(err);
       }
       payload = JSON.parse(payload.toString());
       if (options.verbose) {
         server.log(['debug', 'hapi-micro-mail'], { message: 'micro-mail server responded', data: payload });
       }
       if (typeof done === 'function') {
+        // handle all HTTP-level errors:
         if (response.statusCode !== 200) {
+          return done(Boom.create(response.statusCode, response.statusMessage, payload));
+        }
+        // handle SMTP-level errors (return codes that are not '250')
+        // if there were multiple submissions sent with sendIndividual:
+        if (Array.isArray(payload.result)) {
+          // see if any were not successful:
+          let status = 'ok';
+          payload.result.forEach((result) => {
+            if (result.response.indexOf('250') < 0) {
+              status = 'error';
+            }
+          });
+          if (status !== 'ok') {
+            return done({
+              status,
+              message: 'failed to send one or more destination emails',
+              result: payload.result // will contain list of accepted/rejected emails
+            });
+          }
+        }
+        // if only one submission or sendIndividual was not true:
+        if (payload.result.response.indexOf('250') < 0) {
           return done({
             status: 'error',
-            message: payload.message ? payload.message : response.statusMessage,
-            result: payload.result ? payload.result : response.statusCode
+            message: payload.message,
+            result: payload.result
           });
         }
-        return done(payload);
+        return done(null, payload);
       }
     });
   });
